c5e69071889514d808441d5bd17f0fc338a9a22e
Mrinal Purohit <mrinal1892005@gmail.com>
2016-06-17 16:56:24 +0530

    add google analytics

--- conf.py
+++ conf.py
@@ -925,6 +925,16 @@ PRETTY_URLS = True
 # like this:
 EXTRA_HEAD_DATA = """
 <script src="https://use.fontawesome.com/fa20058d35.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-64263019-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 """
 # """<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css">"""
 # USE_KATEX = False
